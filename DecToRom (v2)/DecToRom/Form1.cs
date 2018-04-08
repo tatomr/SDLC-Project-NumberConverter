930f3f41002e4775b64f0bec84ca7a923c0f221a
tatomr <34355847+tatomr@users.noreply.github.com>
2018-04-07 23:01:10 -0400

    Add files via upload

--- DecToRom (v2)/DecToRom/Form1.cs
+++ DecToRom (v2)/DecToRom/Form1.cs
@@ -19,6 +19,7 @@ namespace DecToRom
 
         private void decToRomRadioButton_CheckedChanged(object sender, EventArgs e)
         {
+            // Set properties for the First Radio Button
             arabicEnterLB.Text = "Enter a number between 1 and 3999:";
             romanEnterLB.Text = "Roman Numeral Equivalent:";
             inputTextBox.Text = "";
@@ -30,6 +31,7 @@ namespace DecToRom
 
         private void romToDecRadioButton_CheckedChanged(object sender, EventArgs e)
         {
+            //Set properties for the second radio button
             arabicEnterLB.Text = "Enter a Roman Numerals number between I and MMMCMXCIX:";
             romanEnterLB.Text = "Decimal Equivalent:";
             inputTextBox.Text = "";
@@ -47,43 +49,56 @@ namespace DecToRom
         {
             try
             {
+                //If first radio button is selected
                 if (decToRomRadioButton.Checked)
                 {
+                    //Check the input value to make sure it meet the condition, if true display MessageBox
                     if ((int.Parse(inputTextBox.Text) >= 4000))
                     {
                         MessageBox.Show("Please Choose a Smaller Number");
                     }
+                    // Else if condition is false, run the convertion code
                     else
                     {
 
                         //string Variable
                         string Roman;
                         string inputArabic = inputTextBox.Text;
-
+                        
+                        //Convert the Arabic number using the ArabicConverter Method and set output to Roman Variable
                         Roman = NumberConverter.ArabicConverter(int.Parse(inputTextBox.Text));
                         listBox1.Items.Add(inputArabic + " = " + Roman);
                     }
 
                 }
+                //If second radio button is selected
                 else if (romToDecRadioButton.Checked)
                 {
+                    // String Variable
                     int Arabic;
                     string inputRoman = inputTextBox.Text;
+
+                    //Convert the Roman number using the RomanConverter Method and set output to Arabic Variable
                     Arabic = NumberConverter.RomanConverter(inputTextBox.Text);
                     listBox1.Items.Add(inputRoman  + " = " + Arabic);
 
                 }
+                //If third radio button is selected
                 else if (referenceRadioButton.Checked)
                 {
+                    // Clear anything listed in the listbox 
                     listBox1.Items.Clear();
-
+                    
+                    // Create Variables
                     int counter;
                     string Conversion;
                     string output;
                     counter = 1;
 
+                    //While loop used to create the list of Roman to Arabic Convertions up to 3999
                     while (counter <= 3999)
                     {
+                        //Use the ArabicConverter method to convert the counter number up to 3999
                         Conversion = NumberConverter.ArabicConverter(counter);
                         output = Conversion + " = " + counter;
                         listBox1.Items.Add(output);
@@ -95,7 +110,7 @@ namespace DecToRom
             }
             catch (Exception ex)
             {
-                //Display am error message
+                //Display the error message and clears the input textbox
                 inputTextBox.Text = "User Syntax Error, Unable to Convert! ";
                 inputTextBox.BackColor = Color.Red;
                 MessageBox.Show(ex.Message);
@@ -124,7 +139,7 @@ namespace DecToRom
 
         private void referenceRadioButton_CheckedChanged(object sender, EventArgs e)
         {
-
+            // Set Properties for the third radio button
             arabicEnterLB.Text = "";
             romanEnterLB.Text = "";
             inputTextBox.Text = "Press the Convert Button to view the Roman Numeral Chart";
@@ -134,6 +149,7 @@ namespace DecToRom
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //Clear the items in the list box
             listBox1.Items.Clear();
         }
     }
